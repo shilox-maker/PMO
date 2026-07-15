4c8bc8148bde786796764f177296458dc8a77b26
Jaime Renovell <j.renovell@dacsa.com>
2026-07-15 10:25:59 +0200

    fix(backend): disable HSTS in helmet configuration to prevent forced HTTPS redirection in development/pre HTTP environments

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,8 @@ const PORT = process.env.PORT || 5000;
 
 // Global Middlewares
 app.use(helmet({
-  contentSecurityPolicy: false
+  contentSecurityPolicy: false,
+  hsts: false
 }));
 
 const allowedOrigins = process.env.FRONTEND_URL ? [process.env.FRONTEND_URL, 'http://localhost:5173'] : '*';
