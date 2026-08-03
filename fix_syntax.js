d5ac5dd89dc3785cb405d04b81ee9d17ca9fe177
Jaime Renovell <j.renovell@dacsa.com>
2026-08-03 13:16:30 +0200

    chore: limpieza de parches temporales y scripts obsoletos

--- fix_syntax.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('frontend/src/pages/ProjectDetail.jsx', 'utf8');
-content = content.replace(
-  "      `).join('');",
-  "      `).join('')) : '';"
-);
-fs.writeFileSync('frontend/src/pages/ProjectDetail.jsx', content);
--- scratch/check_schema.js
+++
@@ -1,8 +0,0 @@
-const { ProyectoKeyUsers } = require('../backend/models/index');
-
-async function main() {
-  console.log("ProyectoKeyUsers rawAttributes:", Object.keys(ProyectoKeyUsers.rawAttributes));
-  process.exit(0);
-}
-
-main();
